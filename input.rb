9e1cceb594f9911c69e2f6a762965fa8d61b3358
tetsuya <110829351+tetsuya3656@users.noreply.github.com>
2022-08-17 14:36:26 +0000

    [finish] ruby.lesson6

---
+++ input.rb
@@ -0,0 +1,3 @@
+puts "Rubyの勉強中"
+input_key = gets
+puts "入力された内容は#{input_key}"
---
+++ lesson6-1.rb
@@ -0,0 +1,9 @@
+puts "計算をはじめます"
+puts "2つの値を入力してください"
+
+value1 = gets.to_i
+value2 = gets.to_i
+
+puts "計算結果を出力します"
+puts "value1*value2=#{value1*value2}"
+puts "計算を終了します"
